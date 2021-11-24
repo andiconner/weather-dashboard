f88c173baf19580e7a07e5eb40cc11999a8399e9
Andi Conner <andiconner@icloud.com>
2021-11-24 14:39:01 -0600

    current and 5day forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,7 +10,7 @@ var citySearchTerm = document.querySelector("#city-search-term")
 var weatherContainerEl = document.querySelector("#weather-container")
 var currentConditionsEl = document.querySelector("#current-conditions")
 var input = document.querySelector('.input_text');
-var d = new Date();
+
 
 var conditions = [icon, temp, wind, humidity, uv];
 var icon = document.querySelector("#icon")
@@ -18,6 +18,9 @@ var temp = document.querySelector("#temp");
 var wind = document.querySelector("#wind");
 var humidity = document.querySelector("#humidity");
 var uv = document.querySelector("#uv");
+
+
+var cardDeckEl = document.querySelector(".card-deck")
 
 
 
@@ -36,17 +39,45 @@ var formSubmitHandler = function(event) {
 }
 
 // display current  CITY 
-var displayCity = function(city, searchTerm) {
+var displayCity = function(city, searchTerm, fiveDayData) {
     console.log(city);
     console.log(searchTerm);
+    console.log(fiveDayData)
     // clear old content
-    citySearchTerm.textContent = (searchTerm + d); 
+    citySearchTerm.innerHTML = (searchTerm + moment(city.dt,"X").format(' (MM/DD/YYYY)')) + `<img src='http://openweathermap.org/img/wn/${city.weather[0].icon}.png'  />`; 
+
+    weatherContainerEl.innerHTML=`
+    <div id="current-conditions" class="conditions-list">
+   
+    <p id="temp">Temp:<span>${city.main.temp}</span></p>
+    <p id="wind">Wind:<span>${city.wind.speed}</span></p>
+    <p id="Humidity">Humidity:<span>${city.main.humidity}</span></p>
+    <p id="uv">UV:<span>${fiveDayData.current.uvi}</span></p>
+   </div>
+    
+    ` 
+    cardDeckEl.innerHTML="" //need this so it won't repeat
+    for (let i = 1; i < fiveDayData.daily.length-2; i++) {
+        cardDeckEl.innerHTML=  cardDeckEl.innerHTML + `
+    <div class="card bg-dark text-light p-3">
+    <div class="card-block">
+      <h4 class="date card-title text-light">
+        <span>${moment(fiveDayData.daily[i].dt,'X').format('MM/DD/YYYY')}</span>
+      </h4>
+      
+      <p id="temp1">Temp:<span>${fiveDayData.daily[i].temp.day}</span></p>
+      <p id="wind1">Wind:<span>${fiveDayData.daily[i].wind_speed}</span></p>
+      <p id="Humidity1">Humidity:<span>${fiveDayData.daily[i].humidity}</span></p>
+    </div>
+  </div>
+  
+    ` 
+        
+    }
+  
 };
     
 
-// display CURRENT weather conditions
-//var displayCurrentWeatherConditions = function (city, conditions){
-   
 
 
 //}
@@ -57,31 +88,22 @@ var getCurrentCity = function(cityname){
     // make a request to the url
     fetch(apiUrl).then(function(response){
         response.json().then(function(data) {
-            displayCity(data, cityname);
+
+            var fiveDayUrl =`https://api.openweathermap.org/data/2.5/onecall?lat=${data.coord.lat}&lon=${data.coord.lon}&units=imperial&appid=9525d305aa977583448ee5f241e05b87`
+
+             fetch(fiveDayUrl)
+             .then(function(response){
+                 return response.json()
+             })
+             .then(function(fiveDayData){
+                displayCity(data, cityname,fiveDayData);
+             })
+           
             
         });
     });
 };
 
 
-
-
-
 searchFormEl.addEventListener("submit", formSubmitHandler)
 
-//fetch apiUrl for Current Weather conditions
-submitBtn.addEventListener('click', function(conditions){
-    fetch('https://api.openweathermap.org/data/2.5/weather?q=z'+cityname+'&units=imperial&appid=9525d305aa977583448ee5f241e05b87')
-    .then(response => response.json())
-    .then(data => {
-        var iconValue = data['weather']['icon'];
-        var tempValue = data['main']['temp'];
-        var windValue = data['wind']['speed'];
-        var humidityValue = data['main']['humidity'];
-
-        icon.innerHTML = iconValue;
-        temp.innerHTML = tempValue;
-        wind.innerHTML = windValue;
-        humidity.innerHTML = humidityValue;
-});
-})
